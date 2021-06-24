034bc1fc2c49feff66e06cebaa78a27707d4991f
edwin <edwinvasa@gmail.com>
2021-06-23 21:21:08 -0500

    se mapean las tablas como clases y una de las clases contiene llave primaria compuesta

---
+++ src/main/java/com/platzi/platzi/market/persistence/entity/Cliente.java
@@ -0,0 +1,9 @@
+package com.platzi.platzi.market.persistence.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+}
