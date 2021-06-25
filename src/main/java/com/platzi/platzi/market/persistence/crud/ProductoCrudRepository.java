42b1bc81c49ab48f149f255aef185571ce040d33
edwin <edwinvasa@gmail.com>
2021-06-24 22:23:45 -0500

    creacion del primer repository que interactua con la BD

---
+++ src/main/java/com/platzi/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,15 @@
+package com.platzi.platzi.market.persistence.crud;
+
+import com.platzi.platzi.market.persistence.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    // ejemplo de QueryMethods @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByIdCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
